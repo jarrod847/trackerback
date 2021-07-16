3c4463acbf72b3cd81e18864fee50950b5c4e9ff
jarrodskahill <jarrodskahill@gmail.com>
2021-07-16 01:24:09 -0500

    created some helper functions to get tasks

--- api/tasks/task-model.js
+++ api/tasks/task-model.js
@@ -1,1 +1,17 @@
 const db = require("../database/config");
+
+module.exports = {
+  getTask,
+  getTaskById,
+  createTask,
+  deleteTask,
+  updateTask,
+};
+
+function getTask() {
+  return db("tasks");
+}
+
+function getTaskById(id) {
+  return db("tasks").where("id", id).first();
+}
