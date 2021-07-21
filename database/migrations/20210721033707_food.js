7cd83bc411dce65b964fe38899a7a6418056f55c
jarrodskahill <jarrodskahill@gmail.com>
2021-07-20 23:37:30 -0500

    updated the table

--- database/migrations/20210721033707_food.js
+++ database/migrations/20210721033707_food.js
@@ -1,8 +1,12 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("food", (table) => {
+    table.increments("id").unique();
+    table.string("name");
+    table.string("protein");
+    table.string("Carbohydrates");
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("food");
 };
