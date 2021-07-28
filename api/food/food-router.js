86167cde3e599176cb92e4669e0e9fbc3ccb088d
jarrodskahill <jarrodskahill@gmail.com>
2021-07-27 20:17:41 -0500

    added food router to the server

--- api/food/food-router.js
+++ api/food/food-router.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const food = require("./food-model");
 
-router.get("", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const allFood = await food.getFoods();
     res.status(200).json({ message: "Got all Food", Food: allFood });
@@ -10,7 +10,7 @@ router.get("", async (req, res) => {
   }
 });
 
-router.post("", async (req, res) => {
+router.post("/add", async (req, res) => {
   try {
     const addFood = await food.addFood(req.body);
     res.status(200).json({ message: "added food", food: addFood });
@@ -18,3 +18,5 @@ router.post("", async (req, res) => {
     res.status(500).json({ message: "could not add food" });
   }
 });
+
+module.exports = router;
