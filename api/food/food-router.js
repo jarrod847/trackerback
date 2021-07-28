d8fc1b512aa8b93bf6cc55df7fcce230bc47ee32
jarrodskahill <jarrodskahill@gmail.com>
2021-07-27 20:15:47 -0500

    created a new route to add food

--- api/food/food-router.js
+++ api/food/food-router.js
@@ -9,3 +9,12 @@ router.get("", async (req, res) => {
     res.status(500).json({ message: "could not get food", error: e });
   }
 });
+
+router.post("", async (req, res) => {
+  try {
+    const addFood = await food.addFood(req.body);
+    res.status(200).json({ message: "added food", food: addFood });
+  } catch (e) {
+    res.status(500).json({ message: "could not add food" });
+  }
+});
